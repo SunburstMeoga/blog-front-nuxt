f6eaa8957ae6a5769e35975db789a6362f996694
apple <apple@appledeMacBook-Air.local>
2023-06-16 14:29:53 +0800

    image

--- components/Blog/BlogEditTabInformation.vue
+++ components/Blog/BlogEditTabInformation.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="container custom-container">
+        <div class="flex">
+            <div>封面图片</div>
+            <div>
+                <el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/"
+                    :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
+            </div>
+        </div>
         <div class="editor-box mt-10">
             <div class="quill-editor" :content="content" v-quill:myQuillEditor="options" :style="cusStyle"
                 :width="toolBarwidth" @change="handleEditorChange">
@@ -40,9 +50,11 @@ export default {
     },
     data() {
         return {
+            imageUrl: '',
             editorOption: {
                 /* quill options */
                 modules: {
+
                     toolbar: [
                         ['bold', 'italic', 'underline', 'strike'], //加粗，斜体，下划线，删除线
                         ['blockquote', 'code-block'], //引用，代码块
@@ -81,6 +93,21 @@ export default {
 
     },
     methods: {
+        handleAvatarSuccess(res, file) {
+            this.imageUrl = URL.createObjectURL(file.raw);
+        },
+        beforeAvatarUpload(file) {
+            const isJPG = file.type === 'image/jpeg';
+            const isLt2M = file.size / 1024 / 1024 < 2;
+
+            if (!isJPG) {
+                this.$message.error('上传头像图片只能是 JPG 格式!');
+            }
+            if (!isLt2M) {
+                this.$message.error('上传头像图片大小不能超过 2MB!');
+            }
+            return isJPG && isLt2M;
+        },
         createSpan() {
             let spana = document.createElement('span')
             spana.className = 'suffix-counter'
@@ -122,4 +149,30 @@ export default {
     text-align: center;
     font-size: 12px;
 }
-</style>
+
+.avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+}
+
+.avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+}
+
+.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+}
+
+.avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+}</style>
